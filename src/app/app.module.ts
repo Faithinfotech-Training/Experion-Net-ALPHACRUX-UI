aff591755ab68bb01c7a727b9c064e3212d77704
gazni-experion <mohammed.gasni@experionglobal.com>
2022-02-04 09:31:41 +0530

    Adds Reception Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
+import { NgxPaginationModule } from 'ngx-pagination';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { ReceptionComponent } from './home/reception/reception.component';
 import { HomePageComponent } from './home/reception/home-page/home-page.component';
+import { PaymentsComponent } from './home/reception/payments/payments.component';
+import { TransactionsComponent } from './home/reception/transactions/transactions.component';
+import { RegisterPatientComponent } from './home/reception/register-patient/register-patient.component';
+import { UpdatePatientComponent } from './home/reception/update-patient/update-patient.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ReceptionComponent,
-    HomePageComponent
+    HomePageComponent,
+    PaymentsComponent,
+    TransactionsComponent,
+    RegisterPatientComponent,
+    UpdatePatientComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxPaginationModule,
+    Ng2SearchPipeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
