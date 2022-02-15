1372998ff907184c5e9964af918e7484f03bae70
AlfinaSulfikarB <alfina.b@experionglobal.com>
2022-02-15 15:01:35 +0530

    adds patient list in doctor module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './reception/home-page/home-page.component';
@@ -7,6 +7,8 @@ import { RegisterPatientComponent } from './reception/register-patient/register-
 import { TransactionsComponent } from './reception/transactions/transactions.component';
 import { UpdatePatientComponent } from './reception/update-patient/update-patient.component';
 import {AdministratorComponent} from './administrator/administrator.component';
+import { PatientListComponent } from './doctor/patient-list/patient-list.component';
+import { DoctorComponent } from './doctor/doctor.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
@@ -17,6 +19,8 @@ const routes: Routes = [
   { path: 'reception/update-patient', component: UpdatePatientComponent },
   { path: 'app-administrator', component: AdministratorComponent },
   { path: '**', component: AppComponent },
+  {path: 'doctor/patients/list',component: PatientListComponent},
+  {path: 'doctor',component:DoctorComponent}
 ];
 
 @NgModule({
