1ba6cc83f10e3d6689175881a906d913159e52ea
binitha.s <binitha.s@experionglobal.com>
2022-02-15 20:34:49 +0530

    adds functionalities for diagnosis module

--- src/app/doctor/advice/diagnosis/diagnosis.component.ts
+++ src/app/doctor/advice/diagnosis/diagnosis.component.ts
@@ -1,4 +1,9 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { NgForm, NumberValueAccessor } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { DiagnosisService } from '../../../shared/diagnosis.service'
 
 @Component({
   selector: 'app-diagnosis',
@@ -6,10 +11,90 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diagnosis.component.scss']
 })
 export class DiagnosisComponent implements OnInit {
+  patientId:number;
+  StaffId:Number;
+  NgForm=NgForm;
 
-  constructor() { }
+  constructor(public diagnosisService: DiagnosisService,
+    private router: Router,private route: ActivatedRoute,private toastr: ToastrService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {this.patientId = this.route.snapshot.params['patientId'];
+  this.patientId =13;
+  this.StaffId =1;
+    console.log('insiide oninit')
+  if (this.patientId != 0 || this.patientId != null) {
+
+    //Get patient by id
+    this.diagnosisService.getPatientById(this.patientId).subscribe(
+        response =>{
+        console.log(response);
+
+        //Format date
+        var datePipe = new DatePipe("en-UK");
+        let formattedDate:any =datePipe.transform(response.PatientDob, 'yyyy-MM-dd');
+        response.DateOfJoining = formattedDate;
+        //Assign this response to doctorPatientservice formData
+        this.diagnosisService.formData = Object.assign({}, response);
+      },
+      error => {
+        console.log(error);
+      }
+
+    );
+  }
+  }
+//Submit form
+onSubmit(form: NgForm) {
+  console.log(form.value);
+  let addId = this.diagnosisService.formData.PatientId;
+
+  //Insert or update
+  if (addId != 0 || addId != null) {
+
+    //Insert
+    this.insertDoctorNote(form);
+    this.resetForm(form);
+    this.router.navigateByUrl('');
+
+
+  } else {
+
+    console.log('An Error has occured');
+
+
+  }
+}
+
+ //Insert Method
+ insertDoctorNote(form?: NgForm) {
+  console.log("Inserting a record....");
+  this.diagnosisService.insertNote(form.value).subscribe(res => {
+    console.log(res);
+    this.toastr.success('Patient diagnosis Inserted Successfully', 'CMS App V2022');
+  },
+    err => {
+      console.log(err);
+    }
+  );
+}
+
+
+
+//Clear all contents after submit
+resetForm(form?: NgForm) {
+  if (form != null) {
+    form.resetForm();
+  }
+}
   }
 
-}
+
+
+
+
+
+
+
+
+
+
---
+++ src/app/shared/diagnosis.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Diagnosis } from './diagnosis';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DiagnosisService {
+  diagnosis:Diagnosis[];
+  formData:Diagnosis=new Diagnosis();
+
+  constructor(private httpClient:HttpClient) { }
+  //Insert Employee
+ insertNote(diagnosis: Diagnosis): Observable<any> {
+  return this.httpClient.post(
+    environment.apiUrl  + 'doctor/patient/medicalhistory',diagnosis);
+}
+
+  //Get patient by id
+  getPatientById(id: number): Observable<any> {
+    return this.httpClient.get(environment.apiUrl + 'Receptionist/patientid?id=' + id);
+  }
+}
