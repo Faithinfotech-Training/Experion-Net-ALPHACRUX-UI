c234e87bafe8a6b7b66226f94933ac0c9b0457b5
binitha.s <binitha.s@experionglobal.com>
2022-02-17 13:16:53 +0530

    adds billing component

--- src/app/lab-technician/billing/billing.component.ts
+++ src/app/lab-technician/billing/billing.component.ts
@@ -1,4 +1,10 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AppComponent } from 'src/app/app.component';
+import { LabtestService } from '../../shared/labtest.service';
 
 @Component({
   selector: 'app-billing',
@@ -6,10 +12,108 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./billing.component.scss']
 })
 export class BillingComponent implements OnInit {
+  patientId:number;
+  NgForm=NgForm;
+  page:number=1;
 
-  constructor() { }
+  constructor(
+    public labTestService: LabtestService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private toastr: ToastrService,
+    public app:AppComponent) { }
 
   ngOnInit(): void {
+
   }
+
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    let PatientId = this.labTestService.formData.PatientId;
+
+
+    //Insert or update
+    if (PatientId != 0 || PatientId != null) {
+
+      //Insert
+      this.getPatientById(form);
+
+
+    } else {
+
+      console.log('Enter valid Patient Id')
+
+
+    }
+
+
+
+
+
+  }
+
+
+  onClick(form1: NgForm) {
+    console.log(form1.value);
+    let AdviceId = this.labTestService.formData.AdviceId;
+    if (AdviceId != 0 || AdviceId != null) {
+
+      //Insert
+      this.getTest(form1);
+
+
+    } else {
+
+      console.log('Enter valid Patient Id')
+
+
+    }
+
+
+  }
+
+
+  getPatientById(form?: NgForm) {
+    console.log("Finding the patient..");
+    this.labTestService.getPatientById(this.labTestService.formData.PatientId).subscribe(res => {
+      console.log(res);
+
+
+
+      //Format date
+      var datePipe = new DatePipe("en-UK");
+      let formattedDate:any =datePipe.transform(res.ReportDateTime, 'yyyy-MM-dd');
+      res.ReportDateTime = formattedDate;
+       //Assign this response to updatePatientservice formData
+      this.labTestService.formData = Object.assign({}, res);
+
+
+    },
+      err => {
+        console.log(err);
+      }
+    );
+
+  }
+
+  getTest(form?: NgForm) {
+    console.log("Finding the tests..");
+    this.labTestService.getTests(this.labTestService.formData.AdviceId)
+    .subscribe(res => {
+
+      console.log(res);
+      console.log('INSIDE gettest')
+
+      //this.labTestService. formData1 = Object.assign({}, res);
+
+    },
+      err => {
+        console.log(err);
+      }
+    );
+
+  }
+
 
 }
---
+++ src/app/shared/labtests2.spec.ts
@@ -0,0 +1,7 @@
+import { Labtests2 } from './labtests2';
+
+describe('Labtests2', () => {
+  it('should create an instance', () => {
+    expect(new Labtests2()).toBeTruthy();
+  });
+});
