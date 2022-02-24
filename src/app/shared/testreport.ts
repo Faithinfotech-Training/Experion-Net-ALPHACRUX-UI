2a392b4432de61c4fbbeffb8f551e333fd1f5d90
binitha.s <binitha.s@experionglobal.com>
2022-02-24 11:59:33 +0530

    changes to lab technician module

---
+++ src/app/shared/testreport.ts
@@ -0,0 +1,9 @@
+export class Testreport {
+  ReportId:number=0;
+  DateTime:Date=new Date();
+  PatientId:number=0;
+  StaffId:number=0;
+  ReportlistId:number=0;
+  PatientValue:number=0;
+  TestId:number=0;
+}
